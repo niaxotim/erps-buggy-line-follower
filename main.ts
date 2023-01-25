ef0e7d06a1b1c1d312a41681f00285439078fafa
Tim <58301440+niaxotim@users.noreply.github.com>
2023-01-25 11:55:16 +0000

    Blocks

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+let leftSensor = 0
+let rightSensor = 0
+let sensorDifference = 0
 basic.forever(function () {
-	
+    leftSensor = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Left)
+    rightSensor = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Right)
+    sensorDifference = Math.abs(leftSensor - rightSensor)
+    if (sensorDifference > 10) {
+        if (leftSensor > rightSensor) {
+            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 30)
+        } else {
+            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorLeft)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 30)
+        }
+    } else {
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 30)
+    }
 })
